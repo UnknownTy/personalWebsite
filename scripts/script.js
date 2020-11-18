ae4b2d33633430092422e48a6e34806f6bf8301c
Unknown <Ty@gimpel.ca>
2020-11-18 12:13:26 -0800

    Completed Lab 6 of personal website. Updated courses.html to include all courses found in Javascript courseList Array

--- scripts/script.js
+++ scripts/script.js
@@ -12,12 +12,35 @@ const courseList = [
         name: "Database Systems",
     }
 ];
+
 let inputCode = "" ;
 let promptMessage = "Enter a 4-digit code";
+
+
 while (inputCode.length != 4){
     inputCode = window.prompt(promptMessage);
     if (inputCode.length != 4){
         console.log("Invalid Code.");
         promptMessage = "Invalid code, please enter a 4-digit code.";
     }
+}
+
+let found = false;
+
+for (var _item of courseList){
+    console.log(_item);
+    if (_item.code.split(' ')[1] === inputCode){
+        console.log(`Yes, I am taking the course: ${_item.code} - ${_item.name}`);
+        found = true;
+        break; // Break out as we know that we have found the value.
+    }
+}
+
+if (!found){
+    courseList.push({
+        code: `ACIT ${inputCode}`,
+        name: null,
+    })
+    console.log(`ACIT ${inputCode} added to course list.`);
+    console.log(courseList)
 }
