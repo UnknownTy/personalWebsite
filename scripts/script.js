b80e32c905a0e26031afab442ebf75d360b72209
Unknown <Ty@gimpel.ca>
2020-11-25 10:48:12 -0800

    Added verificiation that the input is 4 digits. Put input and outputs into a function for JS.

--- scripts/script.js
+++ scripts/script.js
@@ -6,16 +6,15 @@ function createCourseArray() {
         for (var info of course.children){
             
             if (info.className === 'course-name'){
-                var fullName = info.textContent.trim().split(" - ")
+                var fullName = info.textContent.trim()
                 console.log(fullName)
             } else if (info.className === 'course-time'){
                 var finishDate = info.textContent.trim()
                 console.log(finishDate)
             }
         }
-        var courseFull = { //All of the info on a single course
-            code: fullName[0],
-            name: fullName[1],
+        let courseFull = { //All of the info on a single course
+            code: fullName,
             date: finishDate,
         }
         courseList.push(courseFull) // Add the full set of info to the array
@@ -24,34 +23,41 @@ function createCourseArray() {
 }
 
 const courseList = createCourseArray()
-let inputCode = "" ;
-let promptMessage = "Enter a 4-digit code";
 
+function findCourse(courseList){
+    let inputCode = "wrong" ;
+    let promptMessage = "Enter a 4-digit code";
+    
+    do {
 
-while (inputCode.length != 4){
-    inputCode = window.prompt(promptMessage);
-    if (inputCode.length != 4){
-        console.log("Invalid Code.");
-        promptMessage = "Invalid code, please enter a 4-digit code.";
+        inputCode = window.prompt(promptMessage);
+        console.log(isNaN(parseInt(inputCode), 10))
+        if (inputCode.length != 4 || isNaN(parseInt(inputCode), 10)){
+            console.log("Invalid Code.");
+            promptMessage = "Invalid code, please enter a 4-digit code.";
+        }
+    } while (inputCode.length != 4 || isNaN(parseInt(inputCode), 10))
+    
+    let found = false;
+    
+    for (var _item of courseList){
+        console.log(_item);
+        if (_item.code.split(' ')[1] === inputCode){
+            console.log(`Yes, I am taking the course: ${_item.code} - ${_item.name}`);
+            found = true;
+            break; // Break out as we know that we have found the value.
+        }
+    }
+    
+    if (!found){
+        courseList.push({
+            code: `ACIT ${inputCode}`,
+            name: null,
+            date: null,
+        })
+        console.log(`ACIT ${inputCode} added to course list.`);
+        console.log(courseList)
     }
 }
 
-let found = false;
-
-for (var _item of courseList){
-    console.log(_item);
-    if (_item.code.split(' ')[1] === inputCode){
-        console.log(`Yes, I am taking the course: ${_item.code} - ${_item.name}`);
-        found = true;
-        break; // Break out as we know that we have found the value.
-    }
-}
-
-if (!found){
-    courseList.push({
-        code: `ACIT ${inputCode}`,
-        name: null,
-    })
-    console.log(`ACIT ${inputCode} added to course list.`);
-    console.log(courseList)
-}
+findCourse(courseList)
